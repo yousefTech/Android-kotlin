2ae6db608d44343ed7af7cccad08a33bf060023e
yousefTech <48622416+yousefTech@users.noreply.github.com>
2022-04-04 15:53:22 +0300

    Add files via upload

---
+++ guess.kt
@@ -0,0 +1,23 @@
+package calculator
+
+fun main(){
+    var number = (0..10).random()
+    var guess :Int;var counter = 1
+
+    println("Lets play guess the Number")
+    print("I am ready, ")
+    println("What's  Your guess")
+    guess = readLine()?.toInt()!!
+    do {
+       if (number<guess){
+           println("Low.Guess again")
+        guess = readLine()?.toInt()!!
+       counter++}
+        else if(number>guess){
+           println("High.Guess again")
+           guess = readLine()?.toInt()!!
+           counter++
+        }
+    }while (number!=guess)
+    println("You got it in $counter")
+}
